332423ed2085287fded68b57b6eb22dd1d88d7a7
Isack Shayo <isack.it@gmail.com>
2018-09-24 21:45:42 +0300

    Updated configuration

--- common.py
+++ common.py
@@ -7,26 +7,26 @@ config.read('main.conf')
 
 #notifying phone numbers
 nums = []
-for number in config['NUMBERS']: nums.append(number)
+for number in config['MOTION_NOTIFICATION_NUMBERS']: nums.append(number)
 NOTIFY_GSM_NUMBERS = tuple(nums)
 
 #retry attempts
-REDIAL_COUNT = config['CALL']['Redials']
+REDIAL_COUNT = config['MOTION_NOTIFICATION_SETTINGS']['Redials']
 
 #notification settings
-NOTIFY_GAP = config['NOTIFICATIONS']['NotifyGap']
+NOTIFY_GAP = config['MOTION_NOTIFICATION_SETTINGS']['NotifyGap']
 
 #meye nvr server info
 NVR_SERVER_MAC_ADDRESS = config['SERVER']['ServerMAC']
 NVR_SERVER_IP_ADDRESS = config['SERVER']['ServerIP']
 NVR_SERVER_PORT = config['SERVER']['ServerPort']
 
+#logging settings
 homeSecurityLogger = logging.getLogger('MAINLOG')
 handler=logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter("%(asctime)s %(filename)-12.12s %(levelname)-8s: %(message)s"))
 homeSecurityLogger.addHandler(handler)
 homeSecurityLogger.setLevel(logging.DEBUG)
-
 
 #named pipe for IPC
 FIFO = 'homesecpipe'
